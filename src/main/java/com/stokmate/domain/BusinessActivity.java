b5bd3df3791ffa4bb5ab252a0f4bc050a7bc4840
enesiyidil <enesiyidil16@gmail.com>
2026-01-15 01:45:42 +0300

    Refactor ShipmentController and UserEventController to remove unused imports

--- src/main/java/com/stokmate/domain/BusinessActivity.java
+++ src/main/java/com/stokmate/domain/BusinessActivity.java
@@ -2,7 +2,6 @@ package com.stokmate.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Getter;
